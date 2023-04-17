e310d031e9350dd371916cb4d942aa30ffb0f9c4
yasin-yumrutas <yasinyumrutas0@gmail.com>
2023-04-17 22:38:25 +0300

    C# backend

---
+++ C#/Static_MVC/Static_MVC/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Static_MVC.MvcApplication" Language="C#" %>
---
+++ ConsoleApp_1/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace ConsoleApp_1.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
